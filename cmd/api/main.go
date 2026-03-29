76a60f8dce92fdc0632effaf4e592b6dd59fd1a0
Drakoxw <dragon12xw@gmail.com>
2026-03-29 12:47:11 -0500

    Allow all headers in CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -71,7 +71,7 @@ func middleware(next http.Handler) http.Handler {
 		// CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 
 		if r.Method == http.MethodOptions {
